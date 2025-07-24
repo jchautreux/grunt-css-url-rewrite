3536c10d8f4adb98c7e7f50f5303250a7427ff71
Jean-Christophe Hautreux <jchautreux@prestaconcept.net>
2025-07-24 09:48:37 +0200

    replace usage of deprecated grunt utils by equivalent dependencies

--- tasks/lib/fetch.js
+++ tasks/lib/fetch.js
@@ -7,14 +7,13 @@
  */
 
 // Node libs
-var url = require("url");
-var request = require("request");
-var stream = require("stream");
-var buffers = require("buffers");
+var request = require('request');
+var stream = require('stream');
+var buffers = require('buffers');
 
 // Grunt export wrapper
-exports.init = function(grunt) {
-  "use strict";
+exports.init = function (grunt) {
+  'use strict';
 
   var exports = {};
 
@@ -24,24 +23,28 @@ exports.init = function(grunt) {
    * @param img Remote path, like http://url.to/an/image.png
    * @param done Function to call once done
    */
-  exports.image = function(url, done) {
+  exports.image = function (url, done) {
     var resultBuffer;
     var buffList = buffers();
     var imageStream = new stream.Stream();
 
     imageStream.writable = true;
-    imageStream.write = function(data) { buffList.push(new Buffer(data)); };
-    imageStream.end = function() { resultBuffer = buffList.toBuffer(); };
+    imageStream.write = function (data) {
+      buffList.push(new Buffer(data));
+    };
+    imageStream.end = function () {
+      resultBuffer = buffList.toBuffer();
+    };
 
-    request(url, function(error, response, body) {
-      if(error) {
-        done("Unable to get " + url + ". Error: " + error.message);
+    request(url, function (error, response, body) {
+      if (error) {
+        done('Unable to get ' + url + '. Error: ' + error.message);
         return;
       }
 
       // Bail if we get anything other than 200
-      if(response.statusCode !== 200) {
-        done("Unable to get " + url + " because the URL did not return an image. Status code " + response.statusCode + " received");
+      if (response.statusCode !== 200) {
+        done('Unable to get ' + url + ' because the URL did not return an image. Status code ' + response.statusCode + ' received');
         return;
       }
 
